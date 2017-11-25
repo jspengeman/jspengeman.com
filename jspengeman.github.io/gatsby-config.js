0b7208f5cfad1a0a8a404ec49588e11e64e969fa
Jonathan <jonathan.spengeman@gmail.com>
2017-11-25 17:15:27 -0500

    Initial gatsby project setup.

---
+++ jspengeman.github.io/gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    description: 'Thoughts on software development by Jonathan Spengeman.',
+    social: {
+      email: 'jonathan.spengeman@gmail.com',
+      linkedin: 'https://www.linkedin.com/in/jspengeman/',
+      stackoverflow: 'https://stackoverflow.com/users/8112353/jonathan-spengeman/',
+      instagram: 'https://www.instagram.com/jspengy/',
+      github:  'https://github.com/jspengeman/',
+      twitter: 'https://twitter.com/jspengy'
+    }
+  },
+  plugins: [`gatsby-plugin-react-helmet`],
+}
